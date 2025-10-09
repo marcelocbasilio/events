013a348b519815fbd539ecae5493e7cec4883b0e
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-10-09 09:06:32 -0300

    Created User Details projection.

---
+++ src/main/java/com/mar/events/projections/UserDetailsProjection.java
@@ -0,0 +1,13 @@
+package com.mar.events.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+
+    String getPassword();
+
+    Long getRoleId();
+
+    String getAuthority();
+
+}
