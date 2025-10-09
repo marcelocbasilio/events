efd9185ab7264aea65174c66e5bd7797aa477c91
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-10-09 09:13:48 -0300

    Updated User repository.

--- src/main/java/com/mar/events/repositories/UserRepository.java
+++ src/main/java/com/mar/events/repositories/UserRepository.java
@@ -1,9 +1,22 @@
 package com.mar.events.repositories;
 
 import com.mar.events.entities.User;
+import com.mar.events.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
+            FROM tb_user
+            INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
+            INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+            WHERE tb_user.email = :email
+            """)
+    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
 }
