c99b5afb9fb032bb330905f912a92fb91bb27a29
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-10-09 08:52:50 -0300

    Created User repository.

---
+++ src/main/java/com/mar/events/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mar.events.repositories;
+
+import com.mar.events.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
