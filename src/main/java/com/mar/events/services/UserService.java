8dbbb488ebde02ad4d9d0308f21c710f667775f5
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-10-10 00:12:54 -0300

    Create User service.

---
+++ src/main/java/com/mar/events/services/UserService.java
@@ -0,0 +1,38 @@
+package com.mar.events.services;
+
+import com.mar.events.entities.Role;
+import com.mar.events.entities.User;
+import com.mar.events.projections.UserDetailsProjection;
+import com.mar.events.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
