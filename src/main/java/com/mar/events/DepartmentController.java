5cf2448ea7a1c03cfded5812fdfdd6c6c353d639
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-10-10 09:04:03 -0300

    Created Department controller.

---
+++ src/main/java/com/mar/events/DepartmentController.java
@@ -0,0 +1,27 @@
+package com.mar.events;
+
+import com.mar.events.dtos.DepartmentDTO;
+import com.mar.events.services.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/departments")
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+    @GetMapping
+    public ResponseEntity<List<DepartmentDTO>> findAll() {
+        List<DepartmentDTO> list = departmentService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
