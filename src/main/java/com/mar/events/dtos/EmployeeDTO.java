703ce4d89e2e1a410fd84adbd00ad4a3e0bbc4e2
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-10-09 22:18:12 -0300

    Created Employee dto.

--- src/main/java/com/mar/events/dtos/EmployeeDTO.java
+++ src/main/java/com/mar/events/dtos/EmployeeDTO.java
@@ -1,12 +1,21 @@
 package com.mar.events.dtos;
 
 import com.mar.events.entities.Employee;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class EmployeeDTO {
 
     private Long id;
+
+    @NotBlank(message = "Field required")
     private String name;
+
+    @Email(message = "Invalid e-mail")
     private String email;
+
+    @NotNull(message = "Field required")
     private Long departmentId;
 
     public EmployeeDTO() {
